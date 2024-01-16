b7635b37959397f9556ea2d21804b6cdaab616f4
potocki92 <mateusz.potocki92@gmail.com>
2024-01-16 12:55:31 +0100

    *

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import { Route, Routes } from "react-router-dom";
 import './App.css'
 import About from './pages/About';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
