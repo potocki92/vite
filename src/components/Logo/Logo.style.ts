adb1a04f4a1c05449f9aadc5f1e04d78b0a55318
potocki92 <mateusz.potocki92@gmail.com>
2024-01-17 12:29:41 +0100

    *

--- src/components/Logo/Logo.style.ts
+++ src/components/Logo/Logo.style.ts
@@ -2,7 +2,7 @@ import * as stylex from "@stylexjs/stylex";
 
 const styles = stylex.create({
   logo: (fontSize) => ({
-    fontSize,
+    fontSize: fontSize || "7rem",
     lineHeight: '100%',
     textTransform: 'uppercase',
     fontWeight: 700,
